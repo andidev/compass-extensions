3f6bae4ff6f7e1635965b3375131e2a80765cc75
Anders Steiner <anders.steiner@gmail.com>
2012-08-02 17:19:56 +0200

    essage=new extension jquery-mobile

---
+++ jquery-mobile/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery Mobile 1.1.1 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://jquerymobile.com/download/
+
+Documetation
+	http://jquerymobile.com/demos/1.1.1/
+
+Compass Dependencies
+jQuery
+
+Install With Dependencies
+compass install jquery/latest; compass install jquery-mobile/latest;
+
+"""
+
+welcome_message """
+Source
+	http://jquerymobile.com/download/
+
+Documetation
+	http://jquerymobile.com/demos/1.1.1/
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install jquery/latest;
+
+"""
