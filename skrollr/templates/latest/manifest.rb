530cd195779101c25f28c4dce107c20e85434653
Anders Steiner <anders.steiner@gmail.com>
2012-08-02 17:41:30 +0200

    essage=new extension skrollr

---
+++ skrollr/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "Skrollr 0.4.3 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/Prinzhorn/skrollr
+
+Documetation
+	https://github.com/Prinzhorn/skrollr#documentation
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install skrollr/latest;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/Prinzhorn/skrollr
+
+Documetation
+	https://github.com/Prinzhorn/skrollr#documentation
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
