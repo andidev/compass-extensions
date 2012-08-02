6ce4af7e71ed1d8a79fb7f55dd5ed712e775e057
Anders Steiner <anders.steiner@gmail.com>
2012-08-02 18:08:28 +0200

    essage=new extension Underscore.js

---
+++ underscorejs/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "Underscore.js 1.3.3 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://underscorejs.org/
+
+Documetation
+	http://underscorejs.org/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install underscorejs/latest;
+
+"""
+
+welcome_message """
+Source
+	http://underscorejs.org/
+
+Documetation
+	http://underscorejs.org/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
