4bc382df636dcf4854cc93f21c267db7f1df6cad
Anders Steiner <anders.steiner@gmail.com>
2012-08-02 14:27:17 +0200

    new wxtension: jquerypp

---
+++ jquerypp/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery 1.0b2 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://jquerypp.com/
+
+Documetation
+	http://jquerypp.com/
+
+Compass Dependencies
+jQuery
+
+Install With Dependencies
+compass install jquery/latest; compass install jquerypp/latest;
+
+"""
+
+welcome_message """
+Source
+	http://jquerypp.com/
+
+Documetation
+	http://jquerypp.com/
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install jquery/latest;
+
+"""
