360069332cbfb0c586b934646ec52cd7f3ff36f8
Anders Steiner <anders.steiner@gmail.com>
2012-08-03 20:53:51 +0200

    new extension jquery-validation

---
+++ jquery-validation/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery Validation 1.9.0 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+
+Documetation
+	http://docs.jquery.com/Plugins/Validation
+
+Compass Dependencies
+jQuery
+
+Install With Dependencies
+compass install jquery/latest; compass install jquery-validation/latest;
+
+"""
+
+welcome_message """
+Source
+	http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+
+Documetation
+	http://docs.jquery.com/Plugins/Validation
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install jquery/latest;
+
+"""
