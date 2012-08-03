b8d60754afd9e86e56c3a954b843daea685c9924
Anders Steiner <anders.steiner@gmail.com>
2012-08-03 21:09:23 +0200

    new extension normalize.css

---
+++ normalize-css/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "Normalize.css 2012-07-07 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/necolas/normalize.css
+
+Documetation
+	https://github.com/necolas/normalize.css
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install normalize-css/latest;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/necolas/normalize.css
+
+Documetation
+	https://github.com/necolas/normalize.css
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
