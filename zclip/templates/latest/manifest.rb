cb2551eb62361db8046e0e41f22fe1ed0ff91047
Anders Steiner <anders.steiner@gmail.com>
2012-08-03 20:26:07 +0200

    new extension zclip

---
+++ zclip/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "zClip 1.1.1 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://www.steamdev.com/zclip/#download
+
+Documetation
+	http://www.steamdev.com/zclip/#usage
+
+Compass Dependencies
+jQuery
+
+Install With Dependencies
+compass install jquery/latest; compass install zero-clipboard/latest; compass install zclip/latest;
+
+"""
+
+welcome_message """
+Source
+	http://www.steamdev.com/zclip/#download
+
+Documetation
+	http://www.steamdev.com/zclip/#usage
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install jquery/latest; compass install zero-clipboard/latest;
+
+"""
