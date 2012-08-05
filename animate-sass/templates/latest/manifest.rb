7599a430bda1b17c7569a100f0d654d7fcd643bb
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 01:29:37 +0200

    added new extension animate-sass

---
+++ animate-sass/templates/latest/manifest.rb
@@ -0,0 +1,39 @@
+description "Animate.sass 0.1.1 (latest)"
+
+discover :stylesheets
+discover :css
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/adamstac/animate.sass
+
+Documetation
+	https://github.com/adamstac/animate.sass#usage
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install animate-sass/latest;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/adamstac/animate.sass
+
+Documetation
+	https://github.com/adamstac/animate.sass#usage
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
