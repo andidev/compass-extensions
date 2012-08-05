f8d1017444a42f26783d6413ae8c28c414208977
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 00:11:32 +0200

    fixed bootstrap-data-tables extension

--- bootstrap-data-tables/templates/latest/manifest.rb
+++ bootstrap-data-tables/templates/latest/manifest.rb
@@ -15,10 +15,10 @@ Documetation
 	http://datatables.net/blog/Twitter_Bootstrap_2
 
 Compass Dependencies
-jQuery, Data Tables
+jQuery, Bootstrap, Data Tables
 
 Install With Dependencies
-compass install jquery/latest; compass install data-tables/latest; compass install bootstrap-data-tables/latest;
+compass install jquery/latest; compass install bootstrap/latest; compass install data-tables/latest; compass install bootstrap-data-tables/latest;
 
 """
 
@@ -33,6 +33,6 @@ Compass Dependencies
 jQuery, Data Tables
 
 Install Dependencies
-compass install jquery/latest; compass install data-tables/latest;
+compass install jquery/latest; compass install bootstrap/latest; compass install data-tables/latest;
 
 """
