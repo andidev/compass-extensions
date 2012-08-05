aa01f19b548bbaa336856d5cfd745197a3140a9d
Anders Steiner <anders.steiner@gmail.com>
2012-08-05 19:52:43 +0200

    added new extension jquery-ui

---
+++ jquery-ui/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery Ui 1.8.22 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://jqueryui.com/download
+
+Documetation
+	http://jqueryui.com/demos/
+
+Compass Dependencies
+jQuery
+
+Install With Dependencies
+compass install jquery/latest; compass install jquery-ui/latest;
+
+"""
+
+welcome_message """
+Source
+	http://jqueryui.com/download
+
+Documetation
+	http://jqueryui.com/demos/
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install jquery/latest;
+
+"""
