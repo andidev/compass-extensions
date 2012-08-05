499ee9ee35de874c7d91cf69d786643cee69cb67
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 00:47:57 +0200

    added new extension omg-text

---
+++ omg-text/templates/latest/manifest.rb
@@ -0,0 +1,39 @@
+description "OMG Text 0.0.2 (latest)"
+
+discover :stylesheets
+discover :css
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/jhardy/OMG-TEXT
+
+Documetation
+	http://jaredhardy.com/omg-text/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install omg-text/latest;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/jhardy/OMG-TEXT
+
+Documetation
+	http://jaredhardy.com/omg-text/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
