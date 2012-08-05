c70710c9808b10f21bc8c9fd1d9f207a16cfa587
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 00:12:49 +0200

    added new extension jqtouch

---
+++ jqtouch/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "jQTouch 1.0-b4-rc (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://www.jqtouch.com/
+
+Documetation
+	http://www.jqtouch.com/preview/demos/main/#home
+
+Compass Dependencies
+Zepto or jQuery
+
+Install With Dependencies
+compass install zepto/latest; compass install jqtouch/latest;
+
+"""
+
+welcome_message """
+Source
+	http://www.jqtouch.com/
+
+Documetation
+	http://www.jqtouch.com/preview/demos/main/#home
+
+Compass Dependencies
+jQuery
+
+Install Dependencies
+compass install zepto/latest;
+
+"""
