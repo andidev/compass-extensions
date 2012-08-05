d43d94562e0c19856552a72fdac104ad3ffe421a
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 01:59:24 +0200

    added new extensions cordova

---
+++ cordova-ios/templates/latest/manifest.rb
@@ -0,0 +1,38 @@
+description "Cordova Ios 2.0.0 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://phonegap.com/download
+
+Documetation
+	http://docs.phonegap.com/en/2.0.0/guide_getting-started_ios_index.md.html#Getting%20Started%20with%20iOS
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install cordova-ios/latest;
+
+"""
+
+welcome_message """
+Source
+	http://phonegap.com/download
+
+Documetation
+	http://docs.phonegap.com/en/2.0.0/guide_getting-started_ios_index.md.html#Getting%20Started%20with%20iOS
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
