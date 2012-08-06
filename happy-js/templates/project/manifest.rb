33679c5008e114a442448a8585226a32d0bc7316
Anders Steiner <anders.steiner@gmail.com>
2012-08-06 18:43:57 +0200

    Added new extension happy-js

---
+++ happy-js/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "Happy.js 1.0-b4-rc (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/andyet/Happy.js
+
+Documetation
+	http://happyjs.com/
+
+Compass Dependencies
+Zepto or jQuery
+
+Install With Dependencies
+compass install zepto/latest; compass install happy-js/latest;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/andyet/Happy.js
+
+Documetation
+	http://happyjs.com/
+
+Compass Dependencies
+Zepto or jQuery
+
+Install Dependencies
+compass install zepto/latest;
+
+"""
