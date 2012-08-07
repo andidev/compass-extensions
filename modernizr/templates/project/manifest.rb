e0a7382f33a47210fa49be40b97823593b7074d2
Anders Steiner <anders.steiner@gmail.com>
2012-08-07 21:39:58 +0200

    Added new extension modernizr

---
+++ modernizr/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "Modernizr 2.6.1 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://modernizr.com/download/
+
+Documetation
+	http://modernizr.com/docs/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install modernizr;
+
+"""
+
+welcome_message """
+Source
+	http://modernizr.com/download/
+
+Documetation
+	http://modernizr.com/docs/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
