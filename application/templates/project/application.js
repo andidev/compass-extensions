30367feaf7792ee8e621cf4f994e6858c53c085c
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 13:43:24 +0200

    Added new extension application

---
+++ application/templates/project/application.js
@@ -0,0 +1,12 @@
+// Import extensions
+//@codekit-prepend "modernizr.js"
+//@codekit-prepend "javascript-debug.js"
+//@codekit-prepend "jquery.js"
+//@codekit-prepend "bootstrap.js"
+
+// Import application
+//@codekit-prepend "application/helpers.js"
+//@codekit-prepend "application/common.js"
+//@codekit-prepend "application/header.js"
+//@codekit-prepend "application/page.js"
+//@codekit-prepend "application/footer.js"
---
+++ application/templates/project/manifest.rb
@@ -0,0 +1,40 @@
+description "Application Template"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+#discover :directories
+
+help """
+Source
+	n/a
+
+Documetation
+	n/a
+
+Compass Dependencies
+	Morednizr, Javascript Debug, jQuery, Bootstrap, Font Awesome
+
+Install With Dependencies
+	compass install modernizr; compass install javascript-debug; compass install jquery; compass install bootstrap;  compass install font-awesome;  compass install application;
+
+
+"""
+
+welcome_message """
+Source
+	n/a
+
+Documetation
+	n/a
+
+Compass Dependencies
+	Morednizr, Javascript Debug, jQuery, Morednizr, Bootstrap, Font Awesome
+
+Install Dependencies
+	compass install modernizr; compass install javascript-debug; compass install jquery; compass install bootstrap;  compass install font-awesome;
+
+"""
