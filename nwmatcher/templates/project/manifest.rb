a0b0a25f164ee47d6538640b87844a618faf0ecb
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 11:35:50 +0200

    Added new extension selectvizr (example missing)

---
+++ nwmatcher/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "NWMatcher 1.2.5 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://javascript.nwbox.com/NWMatcher/
+
+Documetation
+	http://javascript.nwbox.com/NWMatcher/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install nwmatcher;
+
+"""
+
+welcome_message """
+Source
+	http://javascript.nwbox.com/NWMatcher/
+
+Documetation
+	http://javascript.nwbox.com/NWMatcher/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
