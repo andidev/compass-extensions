d1f60e63af8cd751ffaf34d52c9d576ac53fa44c
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 10:32:10 +0200

    Added new extension javascript-debug

---
+++ javascript-debug/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "Javascript Debug 0.4 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/cowboy/javascript-debug/tree/v0.4
+
+Documetation
+	http://benalman.com/code/projects/javascript-debug/docs/files/ba-debug-js.html
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install javascript-debug;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/cowboy/javascript-debug/tree/v0.4
+
+Documetation
+	http://benalman.com/code/projects/javascript-debug/docs/files/ba-debug-js.html
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
