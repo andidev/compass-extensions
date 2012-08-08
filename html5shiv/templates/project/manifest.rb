f5754da67f7da44b39c7f378da3ba17cc481b526
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 11:35:17 +0200

    Added new extension html5shiv

---
+++ html5shiv/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "HTML5shiv 3.6 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/aFarkas/html5shiv
+
+Documetation
+	http://code.google.com/p/html5shiv/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install html5shiv;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/aFarkas/html5shiv
+
+Documetation
+	http://code.google.com/p/html5shiv/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
