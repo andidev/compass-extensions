76bd1d18a89456f0030c256e69292e928294b77c
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 14:25:28 +0200

    Added new extension quint

---
+++ qunit/templates/project/manifest.rb
@@ -0,0 +1,40 @@
+description "QUnit 1.9.0 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://qunitjs.com/
+
+Documetation
+	http://api.qunitjs.com/
+	http://net.tutsplus.com/tutorials/javascript-ajax/how-to-test-your-javascript-code-with-qunit/?search_index=4
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install qunit;
+
+"""
+
+welcome_message """
+Source
+	http://qunitjs.com/
+
+Documetation
+	http://api.qunitjs.com/
+	http://net.tutsplus.com/tutorials/javascript-ajax/how-to-test-your-javascript-code-with-qunit/?search_index=4
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
