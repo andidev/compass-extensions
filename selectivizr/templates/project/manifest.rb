a4f03ac4e83b2dca837957e18d7e896d8675b5d6
Anders Steiner <anders.steiner@gmail.com>
2012-08-08 11:37:48 +0200

    Added new extension selectvizr (example missing)

---
+++ selectivizr/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "Selectivizr 1.0.2 (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://selectivizr.com/
+
+Documetation
+	http://selectivizr.com/
+
+Compass Dependencies
+jQuery or Dojo or Prototype or YUI or DOMAssistant or MooTools or NWMatcher
+
+Install With Dependencies
+compass install jquery; compass install selectivizr;
+
+"""
+
+welcome_message """
+Source
+	http://selectivizr.com/
+
+Documetation
+	http://selectivizr.com/
+
+Compass Dependencies
+jQuery or Dojo or Prototype or YUI or DOMAssistant or MooTools or NWMatcher
+
+Install Dependencies
+compass install jquery; compass
+
+"""
