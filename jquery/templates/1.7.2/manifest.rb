691aeda5d2f6fb27a2a524e7f54a91c50f1af252
Anders Steiner <anders.steiner@gmail.com>
2012-08-10 11:24:12 +0200

    Added version 1.7.2 and 1.8.0 to jquery

---
+++ jquery/templates/1.7.2/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery 1.7.2"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	http://jquery.com/download/
+
+Documetation
+	http://api.jquery.com/category/selectors/
+
+Compass Dependencies
+none
+
+Install With Dependencies
+compass install jquery/1.7.2;
+
+"""
+
+welcome_message """
+Source
+	http://jquery.com/download/
+
+Documetation
+	http://api.jquery.com/category/selectors/
+
+Compass Dependencies
+none
+
+Install Dependencies
+n/a
+
+"""
