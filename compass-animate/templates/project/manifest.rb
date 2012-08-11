91b11d457a68f2750f48633f2404c846ee63be5e
Anders Steiner <anders.steiner@gmail.com>
2012-08-11 11:58:19 +0200

    Added new extension compass-animate

---
+++ compass-animate/templates/project/manifest.rb
@@ -0,0 +1,39 @@
+description "Compass Animate 0.1.alpha.0 (latest)"
+
+discover :stylesheets
+discover :css
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/ericam/compass-animate
+
+Documetation
+	https://github.com/ericam/compass-animate#usage
+
+Compass Dependencies
+Compass >= 0.13
+
+Install With Dependencies
+compass install compass-animate;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/ericam/compass-animate
+
+Documetation
+	https://github.com/ericam/compass-animate#usage
+
+Compass Dependencies
+Compass >= 0.13
+
+Install Dependencies
+n/a
+
+"""
