d9292e91a480b268d90c17b55e673511f2d9d679
Anders Steiner <anders.steiner@gmail.com>
2012-09-08 17:38:07 +0200

    Added Bootstrap jQuery Validation

---
+++ bootstrap-jquery-validation/templates/project/manifest.rb
@@ -0,0 +1,38 @@
+description "Bootstrap jQuery Validation n/a (latest)"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/tonycoco/rails_template/blob/master/files/assets/javascripts/jquery.validate.bootstrap.js
+
+Documetation
+	n/a
+
+Compass Dependencies
+	jQuery, Bootstrap, jQuery Validation
+
+Install With Dependencies
+	compass install jquery; compass install bootstrap; compass install jquery-validation; compass install bootstrap-jquery-validation;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/tonycoco/rails_template/blob/master/files/assets/javascripts/jquery.validate.bootstrap.js
+
+Documetation
+	n/a
+
+Compass Dependencies
+	jQuery, Bootstrap, jQuery Validation
+
+Install Dependencies
+	compass install jquery; compass install bootstrap; compass install jquery-validation;
+
+"""
