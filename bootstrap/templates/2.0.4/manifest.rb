669e532f2234d9b6cf1f74758323fe9b7c7e7930
Anders Steiner <anders.steiner@gmail.com>
2012-09-08 09:11:40 +0200

    Added Bootstrap 2.0.4

---
+++ bootstrap/templates/2.0.4/manifest.rb
@@ -0,0 +1,38 @@
+description "bootstrap 2.0.4"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/twitter/bootstrap/tags
+
+Documetation
+	http://twitter.github.com/bootstrap/
+
+Compass Dependencies
+	jQuery
+
+Install With Dependencies
+	compass install jquery; compass install bootstrap/2.0.4;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/twitter/bootstrap/tags
+
+Documetation
+	http://twitter.github.com/bootstrap/
+
+Compass Dependencies
+	jQuery
+
+Install Dependencies
+	compass install jquery;
+
+"""
