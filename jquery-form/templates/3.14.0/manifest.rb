21f1f4b04daf18fac416fe215b32853a0891f54f
Anders Steiner <anders.steiner@gmail.com>
2012-09-08 11:20:58 +0200

    Added jQuery Form 3.14.0

---
+++ jquery-form/templates/3.14.0/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery Form 3.14.0"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/malsup/form/
+
+Documetation
+	http://malsup.com/jquery/form/#getting-started
+
+Compass Dependencies
+	jQuery
+
+Install With Dependencies
+	compass install jquery; compass install jquery-form/3.14.0;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/malsup/form/
+
+Documetation
+	http://malsup.com/jquery/form/#getting-started
+
+Compass Dependencies
+	jQuery
+
+Install Dependencies
+	compass install jquery;
+
+"""
