286f4d8a664cb19da833c7046ecbb995a274c5a2
Anders Steiner <anders.steiner@gmail.com>
2012-09-08 10:53:31 +0200

    Added jQuery Validate 1.10.0

---
+++ jquery-validation/templates/1.10.0/manifest.rb
@@ -0,0 +1,38 @@
+description "jQuery Validation 1.10.0"
+
+discover :stylesheets
+discover :images
+discover :javascripts
+discover :fonts
+discover :html
+discover :files
+
+help """
+Source
+	https://github.com/jzaefferer/jquery-validation/tags
+
+Documetation
+	http://docs.jquery.com/Plugins/Validation
+
+Compass Dependencies
+	jQuery
+
+Install With Dependencies
+	compass install jquery; compass install jquery-validation/1.10.0;
+
+"""
+
+welcome_message """
+Source
+	https://github.com/jzaefferer/jquery-validation/tags
+
+Documetation
+	http://docs.jquery.com/Plugins/Validation
+
+Compass Dependencies
+	jQuery
+
+Install Dependencies
+	compass install jquery;
+
+"""
